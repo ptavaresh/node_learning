1c1ddd8ac8e75fad5ea9402917e0eaa16ed248bd
Pedro Tavares <ptavaresh@gmail.com>
2019-06-21 14:28:52 -0500

    getting static files using relative path

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
+//with relative path
+var path = require('path');
 const port = 3000;
 
-app.use('/assets', express.static(`${__dirname}/site/assets`));
+//app.use('/assets', express.static(`${__dirname}/site/assets`));
+//with relative path
+console.log(path.join(__dirname, '/site/assets'))
+app.use('/assets', express.static(path.join(__dirname, '/site/assets')))
 
 app.get('/', (req, res, next) => {
     res.sendFile(`${__dirname}/site/index.html`);
